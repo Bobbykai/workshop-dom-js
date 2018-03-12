2d663922dc74553bad092b4a88e531b403170802
Johan Teillais <johan.teillais@ynov.com>
2018-03-12 16:32:07 +0100

    Dynamic Buttons xith array static

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 
-function message(a) {
-  console.log(`Hello ${a}, ca va aujourd'hui? `);
+
+var names = ["Quarisse", "SauceBiggie", "Kwoak", "LETEUTEU","LCALECHIBRE"];
+
+
+var generateMessage = names => {
+return `
+  <button onclick="message('${names}')">${names}
+</button>`;
+};
+var container = document.getElementById('container');
+for (let i = 0; i < names.length; i++) {
+  const person = names[i];
+  container.innerHTML += generateMessage(person);
 }
+var message = names  => {
+  console.log(`Hello ${names}, ca va aujourd'hui? `);
+};
